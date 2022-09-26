d2f4c7c6b1a74da51cb0b1b6aabc7c81695f3403
fransjr <francesc@xceed.me>
2022-09-26 20:15:28 +0200

    adds a comment, not compiling

--- src/application/authenticate_user_application.rs
+++ src/application/authenticate_user_application.rs
@@ -13,6 +13,8 @@ pub async fn authenticate_user(
    //I thing I shoudl have in my head that this whole file could become an standalone
     // service with it's own infra, it just needs to connect to the appropriate DBs.
 
+    // CREDENTIALS NEED TO HAVE UUID or I CANT SAVE THE EVENTS WHERE THEY BELONG.
+
     // will do:
     // 1. load user credentials   // Alt: load user without UUID how??? I can't!!!!
     // 2. ensure credentials found // Alt: ensure user found
@@ -20,6 +22,7 @@ pub async fn authenticate_user(
     // 3. validate password/ // Alt: validate password
     // if not, write event B // if not write event B
     // 4. REturn JWT, write event A // return JWT.
+
 
     // 2 bigquestions: 1. Aggregate less Domain Events? (user tried to login with inexistent email address)
     // 2. application using view data and then writing domain events??
