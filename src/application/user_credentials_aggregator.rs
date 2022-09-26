6c3d0376b384c948b3996a78cbc48bcdbc0f4c4d
fransjr <francesc@xceed.me>
2022-09-26 14:16:56 +0200

    aggregator to deal with a single message, so far the infra gets all the messages

--- src/application/user_credentials_aggregator.rs
+++ src/application/user_credentials_aggregator.rs
@@ -1,22 +1,18 @@
-use crate::domain::{EventStoreInterface, User, UserCredentialsView, UserViewRepositoryError, UserViewRepositoryInterface};
+use crate::domain::{EventEnvelope, User, UserCredentialsView, UserViewRepositoryError, UserViewRepositoryInterface};
 use crate::domain::UserEvent::RegisteredUser;
 
 #[tracing::instrument(
 name = "User Credentials aggregator",
-skip(event_store, view_repository)
+skip(view_repository)
 )]
 pub async fn user_credentials_aggregator(
-    event_store:  impl EventStoreInterface<User>,
-    view_repository: impl UserViewRepositoryInterface,
-    last_read_event: i64
+    event: EventEnvelope<User>,
+    view_repository: &impl UserViewRepositoryInterface,
 ) -> Result<(), UserViewRepositoryError>  {
-    let events = event_store.load_all_events(last_read_event).await?;
-    for event in events {
-        match event.payload {
-            RegisteredUser(user_registered_domain_event) => {
-                let user_credentials_view: UserCredentialsView = user_registered_domain_event.into();
-                view_repository.save_view(user_credentials_view).await?;
-            }
+    match event.payload {
+        RegisteredUser(user_registered_domain_event) => {
+            let user_credentials_view: UserCredentialsView = user_registered_domain_event.into();
+            view_repository.save_view(user_credentials_view).await?;
         }
     }
     Ok(())
--- src/infrastructure/controllers/generate_credentials_view_controller.rs
+++ src/infrastructure/controllers/generate_credentials_view_controller.rs
@@ -2,7 +2,7 @@ use actix_web::{HttpResponse, web};
 use sqlx::PgPool;
 use crate::actix::controllers::NoContentResponse;
 use crate::application::user_credentials_aggregator;
-use crate::domain::{UserViewRepositoryError};
+use crate::domain::{EventStoreInterface, UserViewRepositoryError};
 use crate::event_store_postgres::EventStorePostgres;
 use crate::user_view_repository_postgres::UserViewPostgresRepository;
 
@@ -14,10 +14,14 @@ pub async fn generate_credentials_view(
 ) -> Result<HttpResponse, UserViewRepositoryError> {
     let postgre_event_store_repository = EventStorePostgres::new_event_store(&pool);
     let view_postgre_repository = UserViewPostgresRepository{pool: &pool};
-    user_credentials_aggregator(
-        postgre_event_store_repository,
-        view_postgre_repository,
-        0
-    ).await?;
+
+    let events = postgre_event_store_repository.load_all_events(0).await?;
+    for event in events {
+        user_credentials_aggregator(
+            event,
+            &view_postgre_repository,
+        ).await?;
+    }
+
     Ok(HttpResponse::Created().json(NoContentResponse {}))
 }
